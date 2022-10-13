27900cfa48a457f75e144c7eaf8b6f36d3cba09c
Furkan Karagoz <fkaragoz14@gmail.com>
2022-10-13 22:39:10 +0300

    Initial commit. The problem.

---
+++ 448_find_all_numbers_disappeared_in_an_aray/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/*
+Given an array nums of n integers where nums[i] is in the range [1, n]
+return an array of all the integers in the range [1, n] that do not appear in nums.
+*/
+
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+
+    }
+};
+
+Solution sol;
+
+int main(void)
+{
+	cout << "hellow" << endl;
+	return 0;
+}

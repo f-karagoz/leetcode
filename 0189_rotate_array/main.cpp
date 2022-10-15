6bcc0e916983e55fdbad4c1e99ef1d4c43ebe13e
Furkan Karagoz <fkaragoz14@gmail.com>
2022-10-16 02:37:21 +0300

    Initial commit.

---
+++ 0189_rotate_array/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/*
+Given an array, rotate the array to the right
+by k steps, where k is non-negative.
+*/
+
+class Solution
+{
+public:
+	void rotate(vector<int>& nums, int k) {
+
+	}
+};
+
+Solution sol;
+
+int main(void)
+{
+	cout << "hi" << endl;
+	return 0;
+}

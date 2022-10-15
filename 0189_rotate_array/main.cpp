b020fc6e8d2d86f51b17c34b122b11b8af72b310
Furkan Karagoz <fkaragoz14@gmail.com>
2022-10-16 02:53:27 +0300

    O(kn) time, O(1) memory solution. Too slow.

--- 0189_rotate_array/main.cpp
+++ 0189_rotate_array/main.cpp
@@ -10,7 +10,18 @@ by k steps, where k is non-negative.
 class Solution
 {
 public:
-	void rotate(vector<int>& nums, int k) {
+	void rotate(vector<int>& nums, int k)
+	{
+		for (int i = 0; i < k; ++i)
+		{
+			int temp = nums[nums.size() - 1];
+
+			for (int j = nums.size() - 1; j > 0; --j)
+				nums[j] = nums[j - 1];
+
+			nums[0] = temp;
+		}
+			
 
 	}
 };
@@ -19,6 +30,22 @@ Solution sol;
 
 int main(void)
 {
-	cout << "hi" << endl;
+	/*
+	Input: nums = [1,2,3,4,5,6,7], k = 3
+	Output: [5,6,7,1,2,3,4]
+	*/
+
+	vector<int> vTest = { 1,2,3,4,5,6,7 };
+	
+	for (auto x : vTest)
+		cout << x << ", ";
+	cout << endl;
+
+	sol.rotate(vTest,3);
+
+	for (auto x : vTest)
+		cout << x << ", ";
+	cout << endl;
+
 	return 0;
 }

745e6ee047849e6d5027747754ed5578e52fe23a
Furkan Karagoz <fkaragoz14@gmail.com>
2022-10-15 19:48:45 +0300

    Bubble sort and binary seach attempted. Took too long.

--- 448_find_all_numbers_disappeared_in_an_aray/main.cpp
+++ 448_find_all_numbers_disappeared_in_an_aray/main.cpp
@@ -14,12 +14,43 @@ public:
 	{
 		vector<int> result;
 
-		// brute-force
-		// linear search for each i index [0:n]
-		for (int i = 1; i <= nums.size(); ++i)
-			if (find(nums.begin(), nums.end(), i) == end(nums))
-				result.push_back(i);
+		// sort and binary search
+		// buble sort
+		for (int i = 0; i < nums.size() - 1; ++i)
+			for (int j = 0; j < nums.size() - 1 - i; ++j)
+				if (nums[j + 1] < nums[j])
+				{
+					auto temp = nums[j + 1];
+					nums[j + 1] = nums[j];
+					nums[j] = temp;
+				}
 
+		// binary search for each i index
+		for (int i = 1; i <= nums.size(); ++i)
+		{
+			int foundFlag = 0;
+			int low = 0;
+			int high = nums.size() - 1;
+			int mid;
+
+			while (low <= high)
+			{
+				mid = low + (high - low) / 2;
+				if (nums[mid] == i)
+				{
+					foundFlag = 1;
+					break;
+				}
+				else if (nums[mid] > i)
+					high = mid - 1;
+				else
+					low = mid + 1;
+			}
+
+			if (foundFlag == 0)
+				result.push_back(i);
+		}
+		
 		return result;
     }
 };
@@ -30,8 +61,12 @@ int main(void)
 {
 	cout << "hellow" << endl;
 
-	vector<int> vTest = { 5,2,3,4 };
+	vector<int> vTest = { 4,3,2,7,8,2,3,1 };
+	vector<int> res = sol.findDisappearedNumbers(vTest);
+	
+	for (auto x : res)
+		cout << x << ", ";
+	cout << endl;
 
-	sol.findDisappearedNumbers(vTest); 
 	return 0;
 }

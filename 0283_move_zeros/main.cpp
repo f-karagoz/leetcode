edf8bfd69fb816d0f0109cc0c0704109f4929a15
Furkan Karagoz <fkaragoz14@gmail.com>
2022-10-16 15:07:18 +0300

    Initial commit.

---
+++ 0283_move_zeros/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/*
+Given an integer array nums, move all 0's to the end of it 
+while maintaining the relative order of the non-zero elements.
+Note that you must do this in-place without making a copy of the array.
+*/
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+
+    }
+};
+
+Solution sol;
+
+int main(void)
+{
+    cout << "hi" << endl;
+    return 0;
+}

c2624f1e5210c1255e7890c74fd0b4f1833694df
Furkan Karagoz <fkaragoz14@gmail.com>
2022-10-16 17:32:54 +0300

    Initial commit.

---
+++ 0557_reverse_words_in_a_string_iii/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/*
+Given a string s, reverse the order of characters in each word within
+a sentence while still preserving whitespace and initial word order.
+*/
+
+class Solution {
+public:
+	string reverseWords(string s)
+	{
+
+	}
+};
+
+Solution sol;
+
+int main(void)
+{
+	/*
+	Input: s = "Let's take LeetCode contest"
+	Output: "s'teL ekat edoCteeL tsetnoc"
+	*/
+
+	string sTest = "Let's take LeetCode contest";
+	string sResult = sol.reverseWords(sTest);
+
+	return 0;
+}

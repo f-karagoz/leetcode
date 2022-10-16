e178813c5258ec76f2aa6d9e6c1168adbd2cff1b
Furkan Karagoz <fkaragoz14@gmail.com>
2022-10-16 20:01:05 +0300

    Iterating over the string range and two pointer inversing the words.

--- 0557_reverse_words_in_a_string_iii/main.cpp
+++ 0557_reverse_words_in_a_string_iii/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <stdio.h>
 using namespace std;
 
 /*
@@ -8,10 +9,43 @@ a sentence while still preserving whitespace and initial word order.
 */
 
 class Solution {
+
+	// Did not used
+	void reverseString(string& s)
+	{
+		for (int li = 0, ri = s.size() - 1; li < ri; ++li, --ri)
+		{
+			char temp = s[li];
+			s[li] = s[ri];
+			s[ri] = temp;
+		}
+	}
+
 public:
 	string reverseWords(string s)
 	{
+		string result = s;
+		
+		// iterate over range [0,n]
+		for (int i = 0, li = 0; i <= result.size(); ++i)
+		{
+			// seacrh for space or null termination
+			if (result[i] == ' ' || result[i] == '\0')
+			{
+				// two pointer inverse over the word range
+				for (int ri = i - 1; li < ri; ++li, --ri)
+				{
+					char temp = result[li];
+					result[li] = result[ri];
+					result[ri] = temp;
+				}
 
+				// left index points to the next word
+				li = i + 1;
+			}	
+		}
+
+		return result;
 	}
 };
 
@@ -26,6 +60,9 @@ int main(void)
 
 	string sTest = "Let's take LeetCode contest";
 	string sResult = sol.reverseWords(sTest);
+
+	cout << sTest << endl;
+	cout << sResult << endl;
 
 	return 0;
 }
